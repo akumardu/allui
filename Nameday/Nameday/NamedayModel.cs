cae308f713f9950186b5c653957875c2c933223a
Amar Kumar Dubedy <amdubedy@microsoft.com>
2018-12-03 16:26:04 -0800

    First XAML App

---
+++ Nameday/Nameday/NamedayModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nameday
+{
+    public class NamedayModel 
+    {
+        public int Month { get; }
+        public int Day { get; }
+        public IEnumerable<string> Names { get; }
+
+        public NamedayModel(int month, int day, IEnumerable<string> names)
+        {
+            this.Month = month;
+            this.Day = day;
+            this.Names = names;
+        }
+
+        public string NamesAsString => string.Join(",", Names);
+    }
+}

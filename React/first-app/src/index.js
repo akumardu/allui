c1e5284cc84aea6c3c34a69bc0f75281a46395a8
Amar Kumar Dubedy <amdubedy@microsoft.com>
2019-03-16 18:19:13 -0700

    Fetching random user images: React API calls

--- React/first-app/src/index.js
+++ React/first-app/src/index.js
@@ -2,6 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Background extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      pictures: []
+    };
+  }
+
+  componentWillMount() {
+    fetch("https://randomuser.me/api/?results=500")
+    .then(results => {return results.json();})
+    .then(data => {
+      let pictures = data.results.map((pic) => {
+        return (
+          <div key={pic.results}>
+          <img src={pic.picture.medium} alt={pic.picture.medium}/>
+          </div>
+        )
+      })
+      this.setState({pictures: pictures});
+      console.log("state", this.state.pictures);
+    })
+  }
+
+  render(){
+    return (
+      <div className="container2">
+      <div className="container1">
+      {this.state.pictures}
+      </div>
+      </div>
+    )
+  }
+}
+
 // class Square extends React.Component {
 //     render() {
 //       return (
@@ -124,6 +159,7 @@ function calculateWinner(squares) {
             <div>{status}</div>
             <ol>{moves}</ol>
           </div>
+          <Background />
         </div>
       );
     }
